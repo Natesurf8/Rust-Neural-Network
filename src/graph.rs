02f0175e519f1b9ec386854aee13e59485fca1bd
Nathan Peters <natesurf@yahoo.com>
2019-11-16 17:08:20 -0700

    cleaned up graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,5 @@
+/// Takes data points and displays them on a graph.
+/// An initial range for the values the Graph should expect has to be specified, but the the display will zoom to fit in any new points inside the graph
 pub struct Graph {
     points: Vec<[f64; 2]>,
     min_x: f64,
@@ -5,6 +7,8 @@ pub struct Graph {
     min_y: f64,
     max_y: f64,
     render_rect: [f64; 4],
+    border_radius: f64, // square width & height
+    point_radius: f64, // square width & height
 }
 impl Graph {
     pub fn new(render_rect: [f64; 4], value_rect: [f64; 4]) -> Graph {
@@ -15,8 +19,11 @@ impl Graph {
             min_y: value_rect[1],
             max_y: value_rect[3] - value_rect[1],
             render_rect: render_rect,
+            border_radius: 2.0,
+            point_radius: 2.0,
         }
     }
+    /// Graph will scale x and y to fit this point inside the displayed graph
     pub fn add_data_point(&mut self, point: [f64; 2]) {
         self.points.push(point);
         if point[0] < self.min_x {
@@ -35,13 +42,21 @@ impl Graph {
 
     pub fn draw(&self, r: &piston::RenderArgs, gl: &mut opengl_graphics::GlGraphics) {
         gl.draw(r.viewport(), |c, gl| {
-            let line_color: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+            //draws the border
+            let border_color: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+            graphics::rectangle(border_color, [self.render_rect[0], self.render_rect[1], self.render_rect[2], self.border_radius], c.transform, gl);
+            graphics::rectangle(border_color, [self.render_rect[0], self.render_rect[1], self.border_radius, self.render_rect[3]], c.transform, gl);
+            graphics::rectangle(border_color, [self.render_rect[0], self.render_rect[1] + self.render_rect[3], self.render_rect[2], self.border_radius], c.transform, gl);
+            graphics::rectangle(border_color, [self.render_rect[0] + self.render_rect[2], self.render_rect[1], self.border_radius, self.render_rect[3]], c.transform, gl);
+
+            //graphs all the points
+            let line_color: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
             for p in &self.points {
-                let r: f64 = 10.0;
-                let x: f64 = (p[0] - self.min_x)/(self.max_x - self.min_x) * self.render_rect[2] + self.render_rect[0];
-                let y: f64 = -(p[1] - self.min_y)/(self.max_y - self.min_y) * self.render_rect[3] + self.render_rect[1] + self.render_rect[3];
-                println!("x: {}, y: {}", x, y);
-                graphics::rectangle(line_color, [x-r, y-r, r*2.0, r*2.0], c.transform, gl)
+                let x: f64 = (p[0] - self.min_x)/(self.max_x - self.min_x) * (self.render_rect[2] - self.point_radius*2.0 - self.border_radius) + self.render_rect[0] + self.border_radius;
+                let y: f64 = -(p[1] - self.min_y)/(self.max_y - self.min_y) * (self.render_rect[3] - self.point_radius*2.0 - self.border_radius) + self.render_rect[1] + self.render_rect[3] - 2.0*self.point_radius;
+                let r = self.point_radius;
+                graphics::rectangle(line_color, [x, y, r*2.0, r*2.0], c.transform, gl)
             }
         });
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+mod graph;
+use graph::Graph;
+
+
 fn main() {
+    let gl_version = opengl_graphics::OpenGL::V3_2;
+    let mut window: glutin_window::GlutinWindow = piston::window::WindowSettings::new(
+            "neural_net",
+            [800, 600]
+        )
+        .graphics_api(gl_version)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut gl = opengl_graphics::GlGraphics::new(gl_version);
+
+    // this will measure the performance of the network in real time
+    let mut performance_graph = Graph::new([200.0, 200.0, 500.0, 300.0], [3.0, 3.0, 1.0, 1.0]);
+
+    // try graphing some dummy data
+    for x in 0..=20 {
+        performance_graph.add_data_point([x as f64, ((x-10)*(x-10)-5) as f64])
+    }
+
+    //todo: is this traitwise methodology necessary
+    let mut events = piston::Events::new(piston::EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        use piston::RenderEvent;
+        if let Some(r) = e.render_args() {
+            //draw(&r, &mut gl);
+            performance_graph.draw(&r, &mut gl);
+        }
+    }
     println!("Hello, world!");
 }
